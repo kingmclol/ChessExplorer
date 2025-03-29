c1e5247872c7c09a11678e1f086a8c185f524757
justinGLC <guanlinjc@gmail.com>
2025-03-29 19:27:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from ChessData import ChessData
 from Traverser import Traverser
 
 openings_database = get_openings("data/openings", 3)
-print("Done")
 
 # games_database = read_pgn(["data/games/lichess_tournament_2025.03.26_G0j0ZKLB_2000-superblitz (1).pgn",
 #                            "data/games/lichess_tournament_2025.03.28_bHUDi9Ci_daily-rapid.pgn",
@@ -18,7 +17,6 @@ print("Done")
 #                            "data/games/lichess_tournament_2025.03.25_tqzGNmOv_daily-bullet.pgn"]
 #                           )
 games_database = read_pgn(["data/games/lichess_tournament_2025.03.26_G0j0ZKLB_2000-superblitz (1).pgn"])
-print("Done")
 
 tree = MoveTree("", data=ChessData([], games_database))
 for move_sequence in openings_database:
