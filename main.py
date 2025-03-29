78e51c6fca20ff969f58a728515d5b14f637fc03
justinGLC <guanlinjc@gmail.com>
2025-03-29 19:30:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,7 @@ for move_sequence in openings_database:
     tree.insert_sequence(list(move_sequence), games_database, openings_database)
 
 print("\n=== Welcome to the Chess Opening Explorer ===")
-print("You can navigate through chess openings, view statistics, and explore move trees 
-       under different time controls.\n")
+print("You can navigate through chess openings, view statistics, and explore move trees under different time controls.\n")
 print("Type 'help' to view a list of available commands.\n")
 print("Starting Traverser...\n")
 
